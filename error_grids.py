be5ab5faafc09b98e607663ac12eb282c7c5859d
Stan Kriventsov <stan@kriventsov.com>
2019-09-23 16:10:55 -0700

    Create error_grids.py

---
+++ error_grids.py
@@ -0,0 +1,148 @@
+"""
+Implementation of Clarke and Parkes error grid analysis
+"""
+
+import numpy as np
+
+def clarke_error_zone_detailed(act, pred):
+    """
+    This function outputs the Clarke Error Grid region (encoded as integer)
+    for a combination of actual and predicted value
+
+    Based on 'Evaluating clinical accuracy of systems for self-monitoring of blood glucose':
+    https://care.diabetesjournals.org/content/10/5/622
+    """
+    # Zone A
+    if (act < 70 and pred < 70) or abs(act - pred) < 0.2 * act:
+        return 0
+    # Zone E - left upper
+    if act <= 70 and pred >= 180:
+        return 8
+    # Zone E - right lower
+    if act >= 180 and pred <= 70:
+        return 7
+    # Zone D - right
+    if act >= 240 and 70 <= pred <= 180:
+        return 6
+    # Zone D - left
+    if act <= 70 <= pred <= 180:
+        return 5
+    # Zone C - upper
+    if 70 <= act <= 290 and pred >= act + 110:
+        return 4
+    # Zone C - lower
+    if 130 <= act <= 180 and pred <= (7/5) * act - 182:
+        return 3
+    # Zone B - upper
+    if act < pred:
+        return 2
+    # Zone B - lower
+    return 1
+
+def parkes_error_zone_detailed(act, pred, diabetes_type):
+    """
+    This function outputs the Parkes Error Grid region (encoded as integer)
+    for a combination of actual and predicted value
+    for type 1 and type 2 diabetic patients
+
+    Based on the article 'Technical Aspects of the Parkes Error Grid':
+    https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3876371/
+    """
+    def above_line(x_1, y_1, x_2, y_2, strict=False):
+        if x_1 == x_2:
+            return False
+
+        y_line = ((y_1 - y_2) * act + y_2 * x_1 - y_1 * x_2) / (x_1 - x_2)
+        return pred > y_line if strict else pred >= y_line
+
+    def below_line(x_1, y_1, x_2, y_2, strict=False):
+        return not above_line(x_1, y_1, x_2, y_2, not strict)
+
+    def parkes_type_1(act, pred):
+        # Zone E
+        if above_line(0, 150, 35, 155) and above_line(35, 155, 50, 550):
+            return 7
+        # Zone D - left upper
+        if (pred > 100 and above_line(25, 100, 50, 125) and
+                above_line(50, 125, 80, 215) and above_line(80, 215, 125, 550)):
+            return 6
+        # Zone D - right lower
+        if (act > 250 and below_line(250, 40, 550, 150)):
+            return 5
+        # Zone C - left upper
+        if (pred > 60 and above_line(30, 60, 50, 80) and
+                above_line(50, 80, 70, 110) and above_line(70, 110, 260, 550)):
+            return 4
+        # Zone C - right lower
+        if (act > 120 and below_line(120, 30, 260, 130) and below_line(260, 130, 550, 250)):
+            return 3
+        # Zone B - left upper
+        if (pred > 50 and above_line(30, 50, 140, 170) and
+                above_line(140, 170, 280, 380) and (act < 280 or above_line(280, 380, 430, 550))):
+            return 2
+        # Zone B - right lower
+        if (act > 50 and below_line(50, 30, 170, 145) and
+                below_line(170, 145, 385, 300) and (act < 385 or below_line(385, 300, 550, 450))):
+            return 1
+        # Zone A
+        return 0
+
+    def parkes_type_2(act, pred):
+        # Zone E
+        if (pred > 200 and above_line(35, 200, 50, 550)):
+            return 7
+        # Zone D - left upper
+        if (pred > 80 and above_line(25, 80, 35, 90) and above_line(35, 90, 125, 550)):
+            return 6
+        # Zone D - right lower
+        if (act > 250 and below_line(250, 40, 410, 110) and below_line(410, 110, 550, 160)):
+            return 5
+        # Zone C - left upper
+        if (pred > 60 and above_line(30, 60, 280, 550)):
+            return 4
+        # Zone C - right lower
+        if (below_line(90, 0, 260, 130) and below_line(260, 130, 550, 250)):
+            return 3
+        # Zone B - left upper
+        if (pred > 50 and above_line(30, 50, 230, 330) and
+                (act < 230 or above_line(230, 330, 440, 550))):
+            return 2
+        # Zone B - right lower
+        if (act > 50 and below_line(50, 30, 90, 80) and below_line(90, 80, 330, 230) and
+                (act < 330 or below_line(330, 230, 550, 450))):
+            return 1
+        # Zone A
+        return 0
+
+    if diabetes_type == 1:
+        return parkes_type_1(act, pred)
+
+    if diabetes_type == 2:
+        return parkes_type_2(act, pred)
+
+    raise Exception('Unsupported diabetes type')
+
+def zone_accuracy(act_arr, pred_arr, mode='clarke', detailed=False, diabetes_type=1):
+    """
+    Calculates the average percentage of each zone based on Clarke or Parkes
+    Error Grid analysis for an array of predictions and an array of actual values
+    """
+    acc = np.zeros(9)
+    if mode == 'clarke':
+        res = clarke_error_zone_detailed(act_arr, pred_arr)
+    elif mode == 'parkes':
+        res = parkes_error_zone_detailed(act_arr, pred_arr, diabetes_type)
+    else:
+        raise Exception('Unsupported error grid mode')
+
+    acc_bin = np.bincount(res)
+    acc[:len(acc_bin)] = acc_bin
+
+    if not detailed:
+        acc[1] = acc[1] + acc[2]
+        acc[2] = acc[3] + acc[4]
+        acc[3] = acc[5] + acc[6]
+        acc[4] = acc[7] + acc[8]
+        acc = acc[:5]
+
+    return acc / sum(acc)
